5a96d6d7c34080648cfa8b722c3b5f847d548956
Matteo Crippa <matteo@boostco.de>
2017-09-23 20:13:51 +0200

    table structure

--- .github/convert.rb
+++ .github/convert.rb
@@ -77,7 +77,7 @@ def output_table(ios_app_link, num_projects)
   date = DateTime.now
   date_display = date.strftime "%B %d, %Y"
 
-  o = "| iOS App | Awesome | Conferences | Updated\n| :-: | :-: | :-: | :-: | :-:\n"
+  o = "| iOS App | Awesome | Conferences | Updated\n| :-: | :-: | :-: | :-:\n"
   o << "| [![Download on the App Store](https://img.shields.io/badge/download-app%20store-ff69b4.svg)](#{ios_app_link}) | "
   o << '[![Awesome](https://cdn.rawgit.com/sindresorhus/awesome/d7305f38d29fed78fa85652e3a63e154dd8e8829/media/badge.svg)](https://github.com/sindresorhus/awesome) | '
   o << "![](https://img.shields.io/badge/conferences-#{num_projects}-orange.svg) | "
