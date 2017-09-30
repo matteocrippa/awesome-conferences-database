4e2e58120540b7042a1f339a47ea7a7744a534ff
Matteo Crippa <matteocrippa@Matteos-MacBook-Pro.local>
2017-09-30 15:19:06 +0200

    push sender

--- .github/pushnotification/sendpush.js
+++ .github/pushnotification/sendpush.js
@@ -1,12 +1,52 @@
 // libs
-const OneSignalClient = require('node-onesignal');
+const OneSignal = require('node-onesignal').default;
+const jsonfile = require('jsonfile');
 
-// TODO: add env var to travisCI
-const client = new OneSignalClient([process.env.ONESIGNAL_APPID], [process.env.ONESIGNAL_KEY]);
+// file name
+const lastFile = 'last.json';
+const jsonFile = 'contents.json';
 
-// send push
-/*
-client.sendNotification('test notification', {
-    included_segments: 'all'
+// get file content
+const lastContent = jsonfile.readFileSync(lastFile);
+
+//console.log(lastContent);
+
+// add env var to travisCI
+const client = new OneSignalClient(process.env.ONESIGNAL_APPID, process.env.ONESIGNAL_KEY);
+
+// read json file
+const json = jsonfile.readFileSync(jsonFile);
+
+// last repo found
+var lastConference = "";
+
+// loop
+var startCounting = false;
+var totalNew = 0;
+json.conferences.forEach(function(item) {
+  //console.log(item);
+  if(startCounting == true) {
+    totalNew++;
+    lastConference = item.homepage;
+  }
+  if(item.homepage == lastContent.last) {
+    startCounting = true;
+  }
 });
-*/
+
+
+
+console.log(totalNew);
+
+if(totalNew > 0) {
+  const message = 'Hey, we have found ' + totalNew+ ' new mobile conference, check them out!';
+  // send push
+  client.sendNotification(message, {
+      included_segments: 'all'
+  });
+  //console.log(message);
+  // add last
+  lastContent.last = lastConference;
+  // write file
+  jsonfile.writeFileSync(lastFile, lastContent);
+}
