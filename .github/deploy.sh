6fec3ce9f998a9016d4b24296801140d7866745a
Matteo Crippa <matteocrippa@Matteos-MacBook-Pro.local>
2017-09-30 15:22:43 +0200

    fix

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -15,8 +15,11 @@ git config user.email "awesomeconferencebot@users.noreply.github.com"
 echo add readme
 git add README.md
 
+echo add last
+git add last.json
+
 echo commit
-git commit -m "[auto] [ci skip] Generate README"
+git commit -m "[auto] [ci skip] Generate README && last"
 
 echo push
 git push --quiet "https://${GH_TOKEN}@github.com/matteocrippa/awesome-mobile-conferences" master:master > /dev/null 2>&1
