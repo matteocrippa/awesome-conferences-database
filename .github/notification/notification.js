7eeb269fba34f08ed396bfe501049bfbf2a6fdf2
Matteo Crippa <matteo@boostco.de>
2017-12-06 18:00:40 +0100

    fix script twitter

--- .github/notification/notification.js
+++ .github/notification/notification.js
@@ -4,7 +4,7 @@ const jsonfile = require('jsonfile');
 const Twitter = require('twitter');
 
 // setup twitter
-const client = new Twitter({
+const clientTwitter = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
@@ -59,7 +59,7 @@ if(newConferences.length > 0) {
     });
 
     // send twitter
-    client.post('statuses/update', {status: message +' #awesomemobileconf'}, function(error, tweet, response) {
+    clientTwitter.post('statuses/update', {status: message +' #awesomemobileconf'}, function(error, tweet, response) {
         if (!error) {
             console.log(tweet);
         } else {
