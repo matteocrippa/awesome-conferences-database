3b9ef4bc77f98d71dd87927e034db05052c3ce96
Matteo Crippa <matteo@boostco.de>
2017-12-19 18:18:41 +0100

    improved script

--- .github/crafter/job.js
+++ .github/crafter/job.js
@@ -18,6 +18,7 @@ const client = new OneSignal(process.env.ONESIGNAL_APPID, process.env.ONESIGNAL_
 // file name
 const lastFile = 'last.json';
 const jsonFile = 'contents.json';
+const newFile = 'new.json';
 
 // get file content
 const lastContent = jsonfile.readFileSync(lastFile);
@@ -39,12 +40,12 @@ json.conferences.forEach(function(item) {
     // convert flags
     item.emojiflag = flag(item.country);
 
-    if(item.country == 'Vietnam') {
+    if(item.country === 'Vietnam') {
       item.emojiflag = '🇻🇳';
     }
 
     //console.log(item);
-    if(startCounting == true) {
+    if(startCounting === true) {
         lastConference = item.homepage;
         newConferences.push(item.title);
         twitterConferences.push({
@@ -56,7 +57,7 @@ json.conferences.forEach(function(item) {
             flag: item.emojiflag
         });
     }
-    if(item.homepage == lastContent.last) {
+    if(item.homepage === lastContent.last) {
         startCounting = true;
     }
 });
@@ -67,7 +68,7 @@ json.conferences.forEach(function(item) {
 if(newConferences.length > 0) {
     var message = '';
 
-    if(newConferences.length == 1) {
+    if(newConferences.length === 1) {
         message = '🎫 ' + newConferences.length + ' new mobile conference ('+ newConferences[0] +'), check it out!';
     } else {
         message = '🎫 ' + newConferences.length + ' new mobile conferences ('+ newConferences.join(", ") +'), check them out!';
