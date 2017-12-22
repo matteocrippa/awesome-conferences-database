72cbedc0e99fb4a92f94d8f84a05148bedd29e80
Matteo Crippa <matteo@boostco.de>
2017-12-22 22:19:18 +0100

    test fix script

--- .github/crafter/job.js
+++ .github/crafter/job.js
@@ -59,7 +59,7 @@ json.conferences.forEach(function(item) {
         item.isnew = false;
     }
 
-    if(item.homepage === lastContent.last && item.startDate === lastContent.date) {
+    if(item.homepage === lastContent.url && item.startDate === lastContent.date) {
         startCounting = true;
     }
 });
